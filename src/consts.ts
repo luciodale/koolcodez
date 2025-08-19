a394838ad73730922097dc2235c0e86ecb37ab05
Lucio D'Alessandro <lda@juxt.pro>
2025-08-20 01:08:27 +0200

    wip

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,14 @@ export const SITE_TITLE = 'Kool Codez';
 export const SITE_DESCRIPTION = 'Creative coding solutions with a cool edge. Building innovative web experiences that stand out.';
 export const SITE_AUTHOR = 'Kool Codez';
 export const SITE_URL = 'https://koolcodez.com';
+
+type TNavLink = {
+	href: string;
+	name: string;
+}
+
+export const NAVBAR_LINKS: TNavLink[] = [
+	{ href: '/', name: 'Home' },
+	{ href: '/blog', name: 'Blog' },
+	{ href: '/contacts', name: 'Contacts' }
+];
