6c68fae370854d10f6baaec26d50005e3c534f8a
Lucio D'Alessandro <lda@juxt.pro>
2026-03-27 00:47:50 +0100

    Improve seo

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
-import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
 import { siteConfig } from '../config/site';
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
-		site: context.site,
+		site: context.site || '',
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
